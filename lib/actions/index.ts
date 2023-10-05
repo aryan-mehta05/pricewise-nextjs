ff165b61a4310b8ccfddbcb7a561f90b826c450b
Aryan Mehta <aryannmehta@gmail.com>
2023-10-05 16:15:04 +0530

    Add: Helper util functions for DB actions

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -6,6 +6,7 @@ import Product from "@/lib/models/product.model";
 import { connectToDB } from "@/lib/mongoose";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "@/lib/utils";
+import { User } from "@/types";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
@@ -47,3 +48,68 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     throw new Error(`Failed to create/update product: ${error.message}`);
   }
 };
+
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
+
+    const product = await Product.findOne({ _id: productId });
+
+    if (!product) return null;
+
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export async function getAllProducts() {
+  try {
+    connectToDB();
+
+    const products = await Product.find();
+
+    return products;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectToDB();
+
+    const currentProduct = await Product.findById(productId);
+
+    if (!currentProduct) return null;
+
+    const similarProducts = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3);
+
+    return similarProducts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// export async function addUserEmailToProduct(productId: string, userEmail: string) {
+//   try {
+//     const product = await Product.findById(productId);
+
+//     if (!productId) return;
+
+//     const userExists = product.users.some((user: User) => user.email === userEmail);
+
+//     if (!userExists) {
+//       product.users.push({ email: userEmail });
+//       await product.save();
+      
+//       const emailContent = await generateEmailBody(product, "WELCOME");
+
+//       await sendEmail(emailContent, [userEmail]);
+//     }
+//   } catch (error) {
+    
+//   }
+// };
