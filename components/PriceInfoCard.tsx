dda1c114d52e31a24f96c6277551b8da66b81957
Aryan Mehta <aryannmehta@gmail.com>
2023-10-05 17:36:34 +0530

    Add: Price info card, product details on product page

--- components/PriceInfoCard.tsx
+++ components/PriceInfoCard.tsx
@@ -6,9 +6,19 @@ interface PriceInfoCardProps {
   value: string;
 }
 
+// Cards that appear on the Products page.
 const PriceInfoCard = ({ title, iconSrc, value }: PriceInfoCardProps) => {
+  function getCardBorderColor(title: string) {
+    if (title === "Current Price") return "#b6dbff";
+    if (title === "Average Price") return "#b5e";
+    if (title === "Highest Price") return "#fcc";
+    if (title === "Lowest Price") return "#beffc5";
+  }
+
+  const borderColor = getCardBorderColor(title);
+
   return (
-    <div className={`price-info_card`}>
+    <div className={`price-info_card border-2 border-[${borderColor}]`}>
       <p className="text-base text-black-100">{title}</p>
 
       <div className="flex gap-1">
